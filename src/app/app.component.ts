94da3f2a0149906ae39d274dd3fafe3e6b96075b
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-05-28 02:27:33 +0300

    fix lint errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  ngOnInit() {
-    debugger;
-    console.log('hello world');
-  }
   title = 'app works!';
 }
