b3e0215104130524e7d7f18bbe2f3bed7af3682d
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-05-28 17:57:18 +0300

    fix e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('ng2-goods-list App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('ng2-goods-list');
   });
 });
