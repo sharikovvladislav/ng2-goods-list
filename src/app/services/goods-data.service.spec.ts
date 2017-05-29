9a931600527c87e752998022058b143fb8b1fcd9
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-05-29 21:54:51 +0300

    try to fix units

--- src/app/services/goods-data.service.spec.ts
+++ src/app/services/goods-data.service.spec.ts
@@ -1,4 +1,4 @@
-import {TestBed, inject} from '@angular/core/testing';
+import {TestBed, async, inject} from '@angular/core/testing';
 
 import {HttpModule, Http, BaseRequestOptions, ResponseOptions, XHRBackend} from '@angular/http';
 import {MockBackend} from '@angular/http/testing';
@@ -24,33 +24,35 @@ describe('GoodsDataService', () => {
   }));
 
   describe('getGoods()', () => {
-    it('should return an Promise<Good[]>',
-      inject([GoodsDataService, XHRBackend], (service: GoodsDataService, mockBackend: MockBackend) => {
-        const mockResponse = [
-          {id: 11, name: 'Mr. Nice'},
-          {id: 12, name: 'Narco'},
-          {id: 13, name: 'Bombasto'},
-          {id: 14, name: 'Celeritas'},
-          {id: 15, name: 'Magneta'},
-          {id: 16, name: 'RubberMan'},
-          {id: 17, name: 'Dynama'},
-          {id: 18, name: 'Dr IQ'},
-          {id: 19, name: 'Magma'},
-          {id: 20, name: 'Tornado'}
-        ];
+    fit('should return an Promise<Good[]>',
+      async(
+        inject([GoodsDataService, XHRBackend], (service: GoodsDataService, mockBackend: MockBackend) => {
+          const mockResponse = [
+            {id: 11, name: 'Mr. Nice'},
+            {id: 12, name: 'Narco'},
+            {id: 13, name: 'Bombasto'},
+            {id: 14, name: 'Celeritas'},
+            {id: 15, name: 'Magneta'},
+            {id: 16, name: 'RubberMan'},
+            {id: 17, name: 'Dynama'},
+            {id: 18, name: 'Dr IQ'},
+            {id: 19, name: 'Magma'},
+            {id: 20, name: 'Tornado'}
+          ];
 
-        mockBackend.connections.subscribe((connection) => {
-          connection.mockRespond(new Response(new ResponseOptions({
-            body: JSON.stringify({data: mockResponse})
-          })));
-        });
-
-        service.getGoods()
-          .then(goods => {
-            expect(goods.length).toEqual(10);
-            expect(goods).toEqual([new Good()]);
+          mockBackend.connections.subscribe((connection) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+              body: JSON.stringify({data: mockResponse})
+            })));
           });
-      })
+
+          service.getGoods()
+            .then(goods => {
+              expect(goods.length).toEqual(10);
+              expect(goods).toEqual([new Good()]);
+            });
+        })
+      )
     );
   });
 });
